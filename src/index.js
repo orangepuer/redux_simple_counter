464b6c0c7ca13bff725702cfe123e4bca20f5e18
orangepuer <rkhpuer@gmail.com>
2020-03-09 16:52:36 +0300

    Implement Counter component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import {bindActionCreators, createStore} from 'redux';
 import reducer from "./reducer";
 import * as actions from "./actions";
+import Counter from "./counter";
 
 const store = createStore(reducer);
 const {dispatch} = store;
 const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
 const update = () => {
-  document.getElementById('counter').innerHTML = store.getState();
+  ReactDOM.render(
+    <Counter
+      counter={store.getState()}
+      inc={inc}
+      dec={dec}
+      rnd={() => {
+        const payload = Math.floor(Math.random()*10);
+        rnd(payload)
+      }} />,
+    document.getElementById('root'));
 };
 
+update();
 store.subscribe(update);
-
-document.getElementById('inc').addEventListener("click", inc);
-document.getElementById('dec').addEventListener("click", dec);
-document.getElementById('rnd').addEventListener("click", () => {
-  const payload = Math.floor(Math.random()*10);
-  rnd(payload);
-});
