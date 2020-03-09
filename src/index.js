e8ec865c86470205cf7583bc35784bcdade418eb
orangepuer <rkhpuer@gmail.com>
2020-03-09 09:46:35 +0300

    Add action creators

--- src/index.js
+++ src/index.js
@@ -15,18 +15,21 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
+const inc = () => ({type: 'INC'});
+
+const dec = () => ({type: 'DEC'});
+
+const rnd = (payload) => ({type: 'RND', payload});
+
 const update = () => {
   document.getElementById('counter').innerHTML = store.getState();
 };
 
 store.subscribe(update);
 
-document.getElementById('inc').addEventListener("click", () => store.dispatch({type: 'INC'}));
-document.getElementById('dec').addEventListener("click", () => store.dispatch({type: 'DEC'}));
+document.getElementById('inc').addEventListener("click", () => store.dispatch(inc()));
+document.getElementById('dec').addEventListener("click", () => store.dispatch(dec()));
 document.getElementById('rnd').addEventListener("click", () => {
   const payload = Math.floor(Math.random()*10);
-  store.dispatch({
-    type: 'RND',
-    payload
-  })
+  store.dispatch(rnd(payload));
 });
