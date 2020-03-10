8c2021d244dd53b4ddd5710574c3df20d22572af
orangepuer <rkhpuer@gmail.com>
2020-03-10 22:37:48 +0300

    Add App component and Integrate react-redux

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {bindActionCreators, createStore} from 'redux';
+import {createStore} from 'redux';
 import reducer from "./reducer";
-import * as actions from "./actions";
-import Counter from "./counter";
+import App from "./components/app";
+import {Provider} from "react-redux";
 
 const store = createStore(reducer);
-const {dispatch} = store;
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
-const update = () => {
-  ReactDOM.render(
-    <Counter
-      counter={store.getState()}
-      inc={inc}
-      dec={dec}
-      rnd={() => {
-        const payload = Math.floor(Math.random()*10);
-        rnd(payload)
-      }} />,
-    document.getElementById('root'));
-};
-
-update();
-store.subscribe(update);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
